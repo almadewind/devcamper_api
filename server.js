cc53d6b649766b9de2f7bade84bf3cc1e85f1839
Mario Jerez <almadewind@gmail.com>
2020-04-30 00:47:27 +0200

    Slugify · GeoCoder

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan = require('morgan');
 const colors = require('colors');
+const errorHandlerror = require('./middleware/error');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -18,20 +18,17 @@ const app = express();
 // Body parser
 app.use(express.json());
 
-// Dev loggin middleware
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandlerror);
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold.underline
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 );
 
