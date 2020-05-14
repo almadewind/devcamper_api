4dcb4ac5c6166de73e3c48349c37a6560b9d8ea9
Mario Jerez <almadewind@gmail.com>
2020-05-14 21:04:50 +0200

    Update User Details

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,6 +58,23 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: user });
 });
 
+// @desc     Update password
+// @route    PUT /api/v1/auth/updatepassword
+// @access   Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  // Check currrent password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Password is incorrect'), 401);
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
 // @desc     Forgot Password
 // @route    POST /api/v1/auth/forgotpassword
 // @access   Public
@@ -150,3 +167,20 @@ const sendTokenResponse = (user, statusCode, res) => {
     .cookie('token', token, options)
     .json({ success: true, token });
 };
+
+// @desc     Update user details
+// @route    POST /api/v1/auth/updatedetails
+// @access   Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({ success: true, data: user });
+});
